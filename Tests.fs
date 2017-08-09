a584449d108e8ca8dbf0746fe27950d41f635f26
Jon Taylor <jont@cakesolutions.net>
2017-08-09 01:08:35 +0100

    Add main entry point

--- Tests.fs
+++ Tests.fs
@@ -1,7 +1,5 @@
 module Tests
 
-#nowarn "988"
-
 open System
 open Xunit
 open FsCheck
@@ -17,35 +15,32 @@ let rec runLengthEnc (xs: 'a list): (int * 'a) list =
   | _ ->
     []
 
-[<Fact>]
-let ``run length encoding round trip``() =
+let runLengthDec (r: (int * 'a) list): 'a list =
+  List.collect (fun t -> List.replicate (fst t) (snd t)) r
 
-  let lowerCaseChar: char Gen = Gen.choose ((int)'a', (int)'z') |> Gen.map (char)
-  let upperCaseChar: char Gen = Gen.choose ((int)'A', (int)'Z') |> Gen.map (char)
-  let numChar: char Gen = Gen.choose ((int)'0', (int)'9') |> Gen.map (char)
+let lowerCaseChar: char Gen = Gen.choose ((int)'a', (int)'z') |> Gen.map (char)
 
-  let runLengthDec (r: (int * 'a) list): 'a list =
-    List.collect (fun t -> List.replicate (fst t) (snd t)) r
+let rleItem: (int * char) Gen =
+  gen {
+    let! n = Gen.choose (1, 20)
+    let! c = lowerCaseChar
+    return n, c
+  }
 
-  let rleItem =
+let rec rleList(size: int): (int * char) list Gen =
+  if (size <= 1) then Gen.map List.singleton rleItem
+  else
     gen {
-      let! n = Gen.choose (1, 20)
-      let! c = lowerCaseChar
-      return n, c
+      let! tail = rleList (size - 1)
+      let (_, c1) = List.head tail
+      let! head = Gen.where (fun t -> snd t <> c1) rleItem
+      return head :: tail
     }
 
-  let rec rleList(size: int): (int * char) list Gen =
-    if (size <= 1) then Gen.map List.singleton rleItem
-    else
-      gen {
-        let! tail = rleList (size - 1)
-        let (_, c1) = List.head tail
-        let! head = Gen.where (fun t -> snd t <> c1) rleItem
-        return head :: tail
-      }
+let genOutput: (int * char) list Gen = Gen.sized rleList
 
-  let genOutput: (int * char) list Gen = Gen.sized rleList
-
+[<Fact>]
+let ``run length encoding round trip``() =
   let prop (r: (int * char) list) = runLengthEnc(runLengthDec(r)) = r
   let arb = Arb.fromGen genOutput
   Prop.forAll arb prop |> Check.QuickThrowOnFailure
