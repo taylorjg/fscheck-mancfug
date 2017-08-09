a4bfe020d5f1af8744ae2e6796ce663b532e42a6
Jon Taylor <jont@cakesolutions.net>
2017-08-09 12:31:21 +0100

    Implement alphaNumChar

--- Tests.fs
+++ Tests.fs
@@ -18,24 +18,23 @@ let rec runLengthEnc (xs: 'a list): (int * 'a) list =
 let runLengthDec (r: (int * 'a) list): 'a list =
   List.collect (fun t -> List.replicate (fst t) (snd t)) r
 
-let lowerCaseChar: char Gen = Gen.choose ((int)'a', (int)'z') |> Gen.map (char)
+let alphaNumChar: char Gen = Gen.elements <| ['a'..'z'] @ ['A'..'Z'] @ ['0'..'9']
 
 let rleItem: (int * char) Gen =
   gen {
     let! n = Gen.choose (1, 20)
-    let! c = lowerCaseChar
+    let! c = alphaNumChar
     return n, c
   }
 
 let rec rleList(size: int): (int * char) list Gen =
   if (size <= 1) then Gen.map List.singleton rleItem
-  else
-    gen {
-      let! tail = rleList (size - 1)
-      let (_, c1) = List.head tail
-      let! head = Gen.where (fun t -> snd t <> c1) rleItem
-      return head :: tail
-    }
+  else gen {
+    let! tail = rleList (size - 1)
+    let (_, c1) = List.head tail
+    let! head = Gen.where (fun t -> snd t <> c1) rleItem
+    return head :: tail
+  }
 
 let genOutput: (int * char) list Gen = Gen.sized rleList
 
