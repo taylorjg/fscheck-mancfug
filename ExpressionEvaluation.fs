9a04967ed2f4a0454363155d69186fcced708f1c
Jon Taylor <jont@cakesolutions.net>
2017-08-11 00:05:38 +0100

    Add expression evaluation example from ScalaCheck book

--- ExpressionEvaluation.fs
+++ ExpressionEvaluation.fs
@@ -10,60 +10,45 @@ open FsCheck
 // Implementation code
 ////////////////////////////////////////////////////////////////////////////////
 
-// trait Expression {
-//   override def toString = show(this)
-// }
+type Expression =
+    | Const of n: int
+    | Add of e1: Expression * e2: Expression
+    | Mul of e1: Expression * e2: Expression
 
-// case class Const(n: Int) extends Expression
+let rec eval = function
+    | Const n -> n
+    | Add (e1, e2) -> eval e1 + eval e2
+    | Mul (e1, e2) -> eval e1 * eval e2
 
-// case class Add(
-//   e1: Expression, e2: Expression
-// ) extends Expression
-
-// case class Mul(
-//   e1: Expression, e2: Expression
-// ) extends Expression
-
-// def eval(e: Expression): Int = e match {
-//   case Const(n) => n
-//   case Add(e1,e2) => eval(e1) + eval(e2)
-//   case Mul(e1,e2) => eval(e1) * eval(e2)
-// }
-
-// def show(e: Expression): String = e match {
-//   case Const(n) => n.toString
-//   case Add(e1,e2) => "("+show(e1)+" + "+show(e2)+")"
-//   case Mul(e1,e2) => "("+show(e1)+" * "+show(e2)+")"
-// }
-
-// def rewrite(e: Expression): Expression = e match {
-//   case Add(e1, e2) if e1 == e2 => Mul(Const(2), e1)
-//   case Mul(Const(0), e) => Const(0)
-//   case Add(Const(1), e) => e
-//   case _ => e
-// }
+let rewrite = function
+    | Add (e1, e2) when e1 = e2 -> Mul (Const 2, e1)
+    | Mul (Const 0, e) -> Const 0
+    | Add (Const 1, e) -> e
+    | e -> e
 
 ////////////////////////////////////////////////////////////////////////////////
 // Custom generators
 ////////////////////////////////////////////////////////////////////////////////
 
-// val genExpr: Gen[Expression] = Gen.sized { sz =>
-//   Gen.frequency(
-//     (sz, genConst),
-//     (sz - math.sqrt(sz).toInt, Gen.resize(sz/2, genAdd)),
-//     (sz - math.sqrt(sz).toInt, Gen.resize(sz/2, genMul))
-//   )
-// }
+let genConst = Gen.choose (0, 10) |> Gen.map Const
 
-// val genConst = Gen.choose(0, 10).map(Const(_))
-
-// val genAdd = for {
-//   e1 <- genExpr; e2 <- genExpr
-// } yield Add(e1, e2)
-
-// val genMul = for {
-//   e1 <- genExpr; e2 <- genExpr
-// } yield Mul(e1, e2)
+#nowarn "40"
+let rec genExpr = Gen.sized <| fun sz ->
+    Gen.frequency [
+        sz, genConst
+        sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genAdd
+        sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genMul
+    ]
+and genAdd = gen {
+        let! e1 = genExpr
+        let! e2 = genExpr
+        return Add (e1, e2)
+    }
+and genMul = gen {
+        let! e1 = genExpr
+        let! e2 = genExpr
+        return Mul (e1, e2)
+    }
 
 // implicit val shrinkExpr: Shrink[Expression] = Shrink({
 //   case Const(n) => shrink(n) map Const
@@ -84,10 +69,12 @@ open FsCheck
 ////////////////////////////////////////////////////////////////////////////////
 
 // val propRewrite = forAll(genExpr) { expr =>
-//   eval(rewrite(expr)) == eval(expr)
-// }
-
-// val propRewrite = forAll(genExpr) { expr =>
 //   val rexpr = rewrite(expr)
 //   (eval(rexpr) ?= eval(expr)) :| "rewritten = "+rexpr
 // }
+
+[<Fact>]
+let propRewrite() =
+  let arb = Arb.fromGen genExpr
+  let p expr = eval (rewrite expr) = eval expr
+  Prop.forAll arb p |> Check.QuickThrowOnFailure
