ea57ac9d731118494688382e2e9b361544a6cb62
Jon Taylor <jont@cakesolutions.net>
2017-08-15 18:45:17 +0100

    Tweaks to playground

--- playground.fsx
+++ playground.fsx
@@ -3,6 +3,7 @@
 open FsCheck
 open Gen
 
+let genInt = Arb.generate<int>
 let genInts = Arb.generate<int list>
 
 printfn "%A" (sample 2 5 genInts)
@@ -22,42 +23,41 @@ printfn "%A" (sample 5 10 (frequency [
                                 50, elements [21..30]
                             ]))
 
-// TODO: cover these QuickCheck things (or their nearest equivalent in FsCheck):
-//
-// listOf
-// suchThat
+printfn "%A" (sample 5 5 (genInt |> where (fun n -> n > 10)))
+
 // label
+
 // collect
+
 // classify
+
 // ==>
+
 // example of too many discarded tests (e.g. list of odd numbers)
 
 // ----------------------------------------------------------------------
 // Functor
 // ----------------------------------------------------------------------
 
-// Given a Gen<'a> return a Gen<'b> by lifting a function of 'a -> 'b
-// let map f g = map f g
-
 let g1 = constant 42 |> map (fun n -> n + 1)
 printfn "%A" <| sample 2 2 g1
-
-// let g1 = constant 42 |> map constant
-// printfn "%A" <| sample 2 2 g1
 
 // ----------------------------------------------------------------------
 // Monad
 // ----------------------------------------------------------------------
 
-let flatMap f g = g >>= f
-// let flatMap f g = gen.Bind (g, f)
+// Trying to implement Gen.oneof using 'map'.
+let oneofv1 (gens: Gen<_> seq) =
+  choose(0, Seq.length gens - 1)
+  |> map (fun index -> Seq.item index gens)
 
-// TODO: find a better example
-let g2 = constant 42 |> flatMap constant
-printfn "%A" <| sample 2 2 g2
+// Implementing Gen.oneof using 'flatMap'.
+let oneofv2 (gens: Gen<_> seq) =
+  choose(0, Seq.length gens - 1)
+  >>= (fun index -> Seq.item index gens)
 
-let g3 = constant 42 >>= constant
-printfn "%A" <| sample 2 2 g3
+let g2 = oneofv2 [constant 1; constant 2; constant 3]
+printfn "%A" <| sample 2 10 g2
 
 // ----------------------------------------------------------------------
 // Applicative
@@ -69,23 +69,37 @@ let gn = Arb.generate<int>
 let gs1 = gn |> map (string)
 let gs2 = gn |> map (fun n -> (string) <| n * 10)
 
-let g4 = map3 fn gn gs1 gs2
-printfn "%A" <| sample 5 10 g4
+let g3 = map3 fn gn gs1 gs2
+printfn "%A" <| sample 5 10 g3
 
-let g5 =
-  gn >>= (fun n ->
-    gs1 >>= (fun s1 ->
-      gs2 |> map (fun s2 ->
-        fn n s1 s2)))
-printfn "%A" <| sample 5 10 g5
-
-let g6 = gen {
+let g4 = gen {
   let! n = gn
   let! s1 = gs1
   let! s2 = gs2
   return fn n s1 s2
 }
+printfn "%A" <| sample 5 10 g4
+
+// Gen<(a -> b -> c -> d)> <*> Gen<a>
+// Gen<(b -> c -> d)> <*> Gen<b>
+// Gen<(c -> d)> <*> Gen<c>
+// Gen<(d)>
+
+let g5 = constant fn <*> gn <*> gs1 <*> gs2
+printfn "%A" <| sample 5 10 g5
+
+// if r is (b -> c -> d)
+// (a -> b -> c -> d)
+// (a -> r)
+// map (a -> r) Gen<a> => Gen<r>
+// map (a -> b -> c -> d) Gen<a> => Gen<(b -> c -> d)>
+
+let dummy1 = map fn gn
+
+let g6 = map fn gn <*> gs1 <*> gs2
 printfn "%A" <| sample 5 10 g6
+
+let dummy2 = fn <!> gn
 
 let g7 = fn <!> gn <*> gs1 <*> gs2
 printfn "%A" <| sample 5 10 g7
