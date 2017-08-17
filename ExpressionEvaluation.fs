9993fb6712877e97fc26d24d02b64552c3aeba82
Jon Taylor <jont@cakesolutions.net>
2017-08-17 19:43:56 +0100

    More use of FsCheck.Xunit

--- ExpressionEvaluation.fs
+++ ExpressionEvaluation.fs
@@ -1,8 +1,8 @@
 module ExpressionEvaluation
 
 open System
-open Xunit
 open FsCheck
+open FsCheck.Xunit
 
 // http://booksites.artima.com/scalacheck/examples/html/ch06.html#sec2
 
@@ -11,20 +11,20 @@ open FsCheck
 ////////////////////////////////////////////////////////////////////////////////
 
 type Expression =
-    | Const of n: int
-    | Add of e1: Expression * e2: Expression
-    | Mul of e1: Expression * e2: Expression
+  | Const of n: int
+  | Add of e1: Expression * e2: Expression
+  | Mul of e1: Expression * e2: Expression
 
 let rec eval = function
-    | Const n -> n
-    | Add (e1, e2) -> eval e1 + eval e2
-    | Mul (e1, e2) -> eval e1 * eval e2
+  | Const n -> n
+  | Add (e1, e2) -> eval e1 + eval e2
+  | Mul (e1, e2) -> eval e1 * eval e2
 
 let rewrite = function
-    | Add (e1, e2) when e1 = e2 -> Mul (Const 2, e1)
-    | Mul (Const 0, e) -> Const 0
-    | Add (Const n, e) when abs n < 5 -> e
-    | e -> e
+  | Add (e1, e2) when e1 = e2 -> Mul (Const 2, e1)
+  | Mul (Const 0, e) -> Const 0
+  | Add (Const 1, e) -> e
+  | e -> e
 
 ////////////////////////////////////////////////////////////////////////////////
 // Custom generators
@@ -34,44 +34,43 @@ let genConst = Gen.choose (0, 10) |> Gen.map Const
 
 #nowarn "40"
 let rec genExpr = Gen.sized <| fun sz ->
-    Gen.frequency [
-        sz, genConst
-        sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genAdd
-        sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genMul
-    ]
+  Gen.frequency [
+    sz, genConst
+    sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genAdd
+    sz - (int) (Math.Sqrt ((float)sz)), Gen.resize (sz/2) genMul
+  ]
 and genAdd = gen {
-        let! e1 = genExpr
-        let! e2 = genExpr
-        return Add (e1, e2)
-    }
+    let! e1 = genExpr
+    let! e2 = genExpr
+    return Add (e1, e2)
+  }
 and genMul = gen {
-        let! e1 = genExpr
-        let! e2 = genExpr
-        return Mul (e1, e2)
-    }
+    let! e1 = genExpr
+    let! e2 = genExpr
+    return Mul (e1, e2)
+  }
 
 let shrinkExpr = function
-    | Const n ->
-        Arb.shrink n |> Seq.map Const
-    | Add (e1, e2) ->
-        Seq.concat [
-            seq [e1; e2]
-            Arb.shrink(e1) |> Seq.map (fun e1' -> Add (e1', e2))
-            Arb.shrink(e2) |> Seq.map (fun e2' -> Add (e1, e2'))
-        ]
-    | Mul (e1, e2) ->
-        Seq.concat [
-            seq [e1; e2]
-            Arb.shrink(e1) |> Seq.map (fun e1' -> Mul (e1', e2))
-            Arb.shrink(e2) |> Seq.map (fun e2' -> Mul (e1, e2'))
-        ]
+  | Const n -> Arb.shrink n |> Seq.map Const
+  | Add (e1, e2) ->
+    Seq.concat [
+      seq [e1; e2]
+      Arb.shrink(e1) |> Seq.map (fun e1' -> Add (e1', e2))
+      Arb.shrink(e2) |> Seq.map (fun e2' -> Add (e1, e2'))
+    ]
+  | Mul (e1, e2) ->
+    Seq.concat [
+      seq [e1; e2]
+      Arb.shrink(e1) |> Seq.map (fun e1' -> Mul (e1', e2))
+      Arb.shrink(e2) |> Seq.map (fun e2' -> Mul (e1, e2'))
+    ]
 
 type CustomArbitraries =
-    static member Expression() =
-        { new Arbitrary<Expression>() with
-            override x.Generator = genExpr
-            override x.Shrinker e = shrinkExpr e
-        }
+  static member Expression() =
+    { new Arbitrary<Expression>() with
+      override x.Generator = genExpr
+      override x.Shrinker e = shrinkExpr e
+    }
 
 Arb.register<CustomArbitraries>() |> ignore
 
@@ -79,8 +78,7 @@ Arb.register<CustomArbitraries>() |> ignore
 // Property test
 ////////////////////////////////////////////////////////////////////////////////
 
-[<Fact>]
-let propRewrite() =
-  Check.Quick <| fun expr ->
-    let rexpr = rewrite expr
-    eval rexpr = eval expr |@ sprintf "rewritten = %A" rexpr
+[<Property>]
+let propRewrite expr =
+  let rexpr = rewrite expr
+  eval rexpr = eval expr |@ sprintf "rewritten = %A" rexpr
--- TheoryExample.fs
+++ TheoryExample.fs
@@ -11,14 +11,10 @@ open FsCheck.Xunit
 [<InlineData(4, 3)>]
 [<InlineData(10, 0)>]
 [<InlineData(0, 10)>]
-let additionCommutative (a: int) (b: int) =
-    Assert.Equal(a + b, b + a)
+let additionCommutative (a: int) (b: int) = Assert.Equal(a + b, b + a)
 
 [<Fact>]
-let ``prop_additionCommutative``() =
-  Check.QuickThrowOnFailure <| fun (a: int) (b: int) ->
-    a + b = b + a
+let propAdditionCommutative() = Check.QuickThrowOnFailure <| fun a b -> a + b = b + a
 
 [<Property>]
-let ``prop_additionCommutative2``(a: int) (b: int) =
-  a + b = b + a
+let propAdditionCommutative2 a b = a + b = b + a
